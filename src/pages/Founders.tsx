e724a47d4cb80177c2f2710e405adc6ba62c0f9a
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-06-15 19:38:45 +0530

    fixed margin error

--- src/pages/Founders.tsx
+++ src/pages/Founders.tsx
@@ -70,8 +70,8 @@ const Founders: React.FC = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-black py-16 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto">
+    <div className="min-h-screen bg-black">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
         <div className="text-center mb-16">
           <h1 className="text-5xl font-bold">
             <span className="text-white">Meet Our </span>
